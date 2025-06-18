3ac317b025d27a8194bc4d851faf04b18a510193
chandu7000 <nadimintichandu700@gmail.com>
2025-06-18 12:17:29 +0530

    first commit

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,23 +1,19 @@
-import React, { Children } from 'react'
-import Sidebar from './Sidebar'
-import Navbar from './Navbar'
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
 
-const Layout = ({Children,showSidebar=false}) => {
+const Layout = ({ children, showSidebar = false }) => {
   return (
-    <div className='min-h-screen'>
-        <div className='flex'>
-            {showSidebar && <Sidebar />}
-        </div>
+    <div className="min-h-screen">
+      <div className="flex">
+        {showSidebar && <Sidebar />}
 
-        <div className='flex-1 flex flex-col'>
-            <Navbar />
+        <div className="flex-1 flex flex-col">
+          <Navbar />
 
-            <main className='flex-1 overflow-y-auto'>
-                {Children}
-            </main>
+          <main className="flex-1 overflow-y-auto">{children}</main>
         </div>
+      </div>
     </div>
-  )
-}
-
-export default Layout
+  );
+};
+export default Layout;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import "stream-chat-react/dist/css/v2/index.css";
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
